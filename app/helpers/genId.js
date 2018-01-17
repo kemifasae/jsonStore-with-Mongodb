c92df095a02baae401fdef0c3caea35489aaaab4
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2018-01-17 10:50:21 +0100

    extend chars array

--- app/helpers/genId.js
+++ app/helpers/genId.js
@@ -1,12 +1,16 @@
 let result = ''
 function randomString() {
   //var result = '';
-  chars = ['1','2','3','4','5','a','b','c','d','e'];
+  let j = 0;
+  chars = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h'
+  ,'i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   for (var i = 4; i > 0; --i){
     result += chars[Math.floor(Math.random() * chars.length)];
   }
-  return result;
+  j++;
+  return result + String(j);
 }
+
 result = randomString();
 
 module.exports = result
